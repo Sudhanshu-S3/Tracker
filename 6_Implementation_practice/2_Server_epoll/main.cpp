52fa7e01f139ee66e7f7feff60f4cafca87e9bf7
Sudhanshu-S3 <sudhanshu.person.1@gmail.com>
2025-12-04 00:02:31 +0530

    Add: epoll visual flow

---
+++ 6_Implementation_practice/2_Server_epoll/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+The Flow :-
+1. Setup the listening port.
+2. Make if "Non-blocking" when these no request.
+3. Create epoll instance.
+4. Make the epoll listen to the port.
+5. Start the epoll Loop
+
+Task of epoll :-
+
+A. Epoll instance will sleep until these is a buzzer goes off.
+B. What event is that like
+C. Adding new task
+D. Or The older task need some thing like read something .
+
+
+*/
+
+
+int main()
+{
+
+    return 0;
+}
